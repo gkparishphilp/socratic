3d628081759641ae1b22a090155e2ad1e332a581
Gk Parish-Philp <gk@gkparishphilp.com>
2019-10-28 14:45:36 -0700

    - question styling

--- app/models/socratic/question.rb
+++ app/models/socratic/question.rb
@@ -14,7 +14,7 @@ module Socratic
 
 
 		def self.uis
-			[ ['Text Box', 'text_box' ], [ 'Text Area', 'text_area'], ['Radio Buttons', 'radio'], ['Check Box', 'check_box' ], ['Check Box Group', 'check_box_group' ], ['Select', 'select' ] ]
+			[ ['Text Box', 'text_box' ], [ 'Text Area', 'text_area'], ['Radio Buttons', 'radio'], ['Check Box', 'check_box' ], ['Check Box Group', 'check_box_group' ], ['Select', 'select' ], ['Date', 'date'], ['Agree Box', 'agree_box'] ]
 		end
 
 
